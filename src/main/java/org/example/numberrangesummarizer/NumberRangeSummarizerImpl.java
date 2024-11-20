2c1d407901fb981dcc368b388f04d645f99c28eb
Bobby Fang <bobbyfang@users.noreply.github.com>
2024-11-20 23:10:48 +0200

    Initial commit: Implement NumberRangeSummarizerImpl with NumberRangeSummarizer interface

---
+++ src/main/java/org/example/numberrangesummarizer/NumberRangeSummarizerImpl.java
@@ -0,0 +1,16 @@
+package org.example.numberrangesummarizer;
+
+import java.util.Collection;
+import java.util.List;
+
+public class NumberRangeSummarizerImpl implements NumberRangeSummarizer{
+    @Override
+    public Collection<Integer> collect(String input) {
+        return List.of();
+    }
+
+    @Override
+    public String summarizeCollection(Collection<Integer> input) {
+        return "";
+    }
+}
