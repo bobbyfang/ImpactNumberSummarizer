0caef9a7d3c64d94e10aa12251b131b89fe6c602
Bobby Fang <bobbyfang@users.noreply.github.com>
2024-11-20 23:14:48 +0200

    Implement collect method to parse comma-separated string, convert valid entries to integers and return them sorted in ascending order. Invalid entries are ignored and logged.

--- src/main/java/org/example/numberrangesummarizer/NumberRangeSummarizerImpl.java
+++ src/main/java/org/example/numberrangesummarizer/NumberRangeSummarizerImpl.java
@@ -1,12 +1,29 @@
 package org.example.numberrangesummarizer;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 
 public class NumberRangeSummarizerImpl implements NumberRangeSummarizer{
     @Override
     public Collection<Integer> collect(String input) {
-        return List.of();
+        ArrayList<Integer> values = new ArrayList<>();
+
+        String[] separatedValues = input.split(",");
+        for (String separatedValue : separatedValues) {
+            try {
+                int value = Integer.parseInt(separatedValue);
+                values.add(value);
+            } catch (NumberFormatException e) {
+                System.out.println(separatedValue + " cannot be converted into integer.");
+            }
+        }
+
+        // Sort values in ascending order
+        Comparator<Integer> ascendingOrder = (v1, v2) -> v1 - v2;
+        values.sort(ascendingOrder);
+        return values;
     }
 
     @Override
