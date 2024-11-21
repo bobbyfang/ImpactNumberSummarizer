4f2ecc534e54f3fe271e53c36ab1c18d674f4191
Bobby Fang <bobbyfang@users.noreply.github.com>
2024-11-21 12:35:46 +0200

    Add initial NumberRangeSummarizerImplTest file with placeholder tests

---
+++ src/test/java/org/example/numberrangesummarizer/NumberRangeSummarizerImplTest.java
@@ -0,0 +1,22 @@
+package org.example.numberrangesummarizer;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberRangeSummarizerImplTest {
+
+    @Test
+    void collect() {
+
+    }
+
+    @Test
+    void summarizeCollection() {
+
+    }
+}
